51509e5d103562648b573898850993ef6aa291d8
ErikMendesRC <erik.mendes19@gmail.com>
2026-02-18 13:59:55 -0300

    fix: removido override de codex na lumina vines block

--- src/main/java/com/dreamsdimensions/mod/block/OwLuminaVinesBlock.java
+++ src/main/java/com/dreamsdimensions/mod/block/OwLuminaVinesBlock.java
@@ -8,14 +8,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
  * Trepadeira luminosa baseada em multiface (estilo glow lichen) para grudar em faces de troncos e folhas.
  */
 public class OwLuminaVinesBlock extends GlowLichenBlock {
-    public static final MapCodec<OwLuminaVinesBlock> CODEC = simpleCodec(OwLuminaVinesBlock::new);
-
     public OwLuminaVinesBlock(BlockBehaviour.Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public MapCodec<OwLuminaVinesBlock> codec() {
-        return CODEC;
     }
 }
