b6ce3bfcd9297ce64c71fd5166842625a94550a5
Erik Mendes <Erik.mendes19@gmail.com>
2026-02-17 22:57:11 -0300

    Diagnose and fix night emissive scheduling on chunk load

--- src/main/java/com/dreamsdimensions/mod/DreamsDimensions.java
+++ src/main/java/com/dreamsdimensions/mod/DreamsDimensions.java
@@ -59,6 +59,7 @@ public class DreamsDimensions {
         modEventBus.addListener(CreativeTabEvents::onBuildCreativeTabContents);
 
         NeoForge.EVENT_BUS.addListener(CommonEvents::onServerStarting);
+        NeoForge.EVENT_BUS.addListener(CommonEvents::onChunkLoad);
         NeoForge.EVENT_BUS.addListener(BrewingRecipesHandler::onRegisterBrewingRecipes);
         NeoForge.EVENT_BUS.addListener(DreamDimensionEffectsHandler::onPlayerTick);
         NeoForge.EVENT_BUS.addListener(DreamReturnAttachmentHandler::onPlayerSetSpawn);
